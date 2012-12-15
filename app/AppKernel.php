8024464dde2f585ff0a25ba7bb483ea8392df138
chema <chema@chema-N55SF>
2012-12-15 17:33:47 +0100

    [sprint 1] added fixtures bundle, execute php composer.phar update to get it READ THE FUCKING API BITCH!!!

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Edukagames\UserBundle\UserBundle(),
             new Edukagames\AdminBundle\AdminBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Edukagames/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Edukagames\UserBundle\DataFixtures\ORM;
+
+use Symfony\Component\Validator\Constraints\Date;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Edukagames\UserBundle\Entity\Alumnos;
+
+class LoadUserData implements FixtureInterface
+{
+	public function load(ObjectManager $manager)
+	{
+		
+		for($i = 1; $i<51; $i++){
+			$alumnoFixture = new Alumnos();
+			$alumnoFixture->setNombre("alumno".$i);
+			$alumnoFixture->setApellidos("Apellidos".$i);
+			$alumnoFixture->setCurso("1");
+			$alumnoFixture->setDiagnostico("Diagnostico");
+			$alumnoFixture->setEdad($i);
+ 			$alumnoFixture->setFechaNacimiento(new \DateTime("now"));
+			$alumnoFixture->setPassword("alumno".$i);
+			$alumnoFixture->setSalt("aleatorio");
+			
+			$manager->persist($alumnoFixture);
+        	$manager->flush();
+		}
+	}
+}
+?>
