7e64c240caa444a94d7ca1f60cc726a4e9a2a595
alfonso <alfonso@AlfonsoDesktop>
2012-12-19 19:14:25 +0100

    [Sprint 1] add login with salt, update fixtures some html code

--- src/Edukagames/AdminBundle/DataFixtures/ORM/LoadAdminData.php
+++ src/Edukagames/AdminBundle/DataFixtures/ORM/LoadAdminData.php
@@ -1,26 +1,41 @@
 <?php
 
 namespace Edukagames\AdminBundle\DataFixtures\ORM;
+use Symfony\Component\Security\Core\Encoder\EncoderFactory;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\Validator\Constraints\Date;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Edukagames\AdminBundle\Entity\Profesor;
 
-class LoadAdminData implements FixtureInterface
-{
-	public function load(ObjectManager $manager)
-	{
-		
-		for($i = 1; $i<51; $i++){
+class LoadAdminData implements FixtureInterface, ContainerAwareInterface {
+	
+	private $container;
+	
+	public function load(ObjectManager $manager) {
+
+		for ($i = 1; $i < 51; $i++) {
+			
 			$profesorFixture = new Profesor();
-			$profesorFixture->setNombre("profesor".$i);
-			$profesorFixture->setPassword("profesor".$i);
+			
+			$pwd = 'profesor'.$i;
+			$salt = md5(time());
+			$encoder = $this->container->get('security.encoder_factory')->getEncoder($profesorFixture);
+			
+			$profesorFixture->setNombre("profesor" . $i);
+			$profesorFixture->setPassword($encoder->encodePassword($pwd,$salt));
+			$profesorFixture->setSalt($salt);
 			
 			$manager->persist($profesorFixture);
 		}
 		$manager->flush();
 	}
+	public function setContainer(ContainerInterface $container = null) {
+		$this->container = $container;
+	}
+
 }
 ?>
--- src/Edukagames/UserBundle/DataFixtures/ORM/LoadAlumnoData.php
+++ src/Edukagames/UserBundle/DataFixtures/ORM/LoadAlumnoData.php
@@ -1,29 +1,46 @@
 <?php
 
 namespace Edukagames\UserBundle\DataFixtures\ORM;
-
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Security\Core\Encoder\EncoderFactory;
 use Symfony\Component\Validator\Constraints\Date;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Edukagames\UserBundle\Entity\Alumno;
 
-class LoadAlumnoData implements FixtureInterface
-{
-	public function load(ObjectManager $manager)
-	{
-		
-		for($i = 1; $i<51; $i++){
+class LoadAlumnoData implements FixtureInterface, ContainerAwareInterface {
+
+	private $container;
+	
+	public function load(ObjectManager $manager) {
+
+		for ($i = 1; $i < 51; $i++) {
 			$alumnoFixture = new Alumno();
-			$alumnoFixture->setNombre("alumno".$i);
-			$alumnoFixture->setApellidos("Apellidos".$i);
+
+			$pwd = 'alumno' . $i;
+			$salt = md5(time());
+			$enconder = $this->container->get('security.encoder_factory')
+					->getEncoder($alumnoFixture);
+
+			$alumnoFixture->setNombre("alumno" . $i);
+			$alumnoFixture->setApellidos("Apellidos" . $i);
 			$alumnoFixture->setCurso("1");
 			$alumnoFixture->setDiagnostico("Diagnostico");
+			$alumnoFixture->setSalt($salt);
 			$alumnoFixture->setFechaNacimiento(new \DateTime("now"));
-			$alumnoFixture->setPassword('alumno'.$i);
-			$alumnoFixture->setUserName("alumno".$i);
+
+			$alumnoFixture->setPassword($enconder->encodePassword($pwd, $salt));
+
+			$alumnoFixture->setUserName("alumno" . $i);
 			$manager->persist($alumnoFixture);
 		}
 		$manager->flush();
 	}
+	public function setContainer(ContainerInterface $container = null) {
+		$this->container = $container;
+
+	}
+
 }
 ?>
