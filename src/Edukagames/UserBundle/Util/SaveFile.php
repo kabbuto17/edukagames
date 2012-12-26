52c72341fbd61b611bf49a09858b8bde3e7c8e28
chema <chema@chema-N55SF>
2012-12-26 18:29:34 +0100

    [sprint 2] create class util SaveFile.php in UserBundle/Util

---
+++ src/Edukagames/UserBundle/Util/SaveFile.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Edukagames\UserBundle\Util;
+
+class SaveFile
+{
+	/**
+	 * @author Chema
+	 * @example SaveFile::saveFile($raizImagen, $_FILES['Alumno_Perfil']['tmp_name']["foto"], $nombreArchivo);
+	 * @param string $destination
+	 * @param string $tmp_filename
+	 * @param string $filename
+	 * 
+	 * @return NULL
+	 */
+    public static function saveFile($destination, $tmp_filename, $filename)
+    {
+    	if(!file_exists($destination)){
+        	mkdir($destination);
+    	}
+        move_uploaded_file($tmp_filename, $destination.'/'.$filename);
+    }
+}
+
+?>
