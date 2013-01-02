5c61fe2ec8347ced8685fc963956d3a0fd1a6205
alfonso <alfonso@AlfonsoDesktop>
2013-01-02 14:21:41 +0100

    [sprint 2] -some changes in search alumno

--- src/Edukagames/AdminBundle/Controller/DefaultController.php
+++ src/Edukagames/AdminBundle/Controller/DefaultController.php
@@ -25,14 +25,14 @@ class DefaultController extends Controller
     		ldd("");
     }
 
-    public function SeachAjaxAction($param){
-    	$em = $this->getDoctrine()->getEntityManager();
-    	$query = $em->createQuery(
-    			'SELECT alumno FROM UserBundle:Alumno alumno 
-    			WHERE alumno.userName 
-    			LIKE :search ORDER BY alumno.userName ASC')->setParameter('search', '%'.$param.'%');
-    	$result = $query->getResult();
+//     public function SeachAjaxAction($param){
+//     	$em = $this->getDoctrine()->getEntityManager();
+//     	$query = $em->createQuery(
+//     			'SELECT alumno FROM UserBundle:Alumno alumno 
+//     			WHERE alumno.userName 
+//     			LIKE :search ORDER BY alumno.userName ASC')->setParameter('search', '%'.$param.'%');
+//     	$result = $query->getResult();
     	
-    	return $this->render('AdminBundle:Default:search.html.twig', array('result' => $result, 'param' => $param));
-    }
+//     	return $this->render('AdminBundle:Default:search.html.twig', array('result' => $result, 'param' => $param));
+//     }
 }
